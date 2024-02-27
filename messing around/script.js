4960742e514ce1a0e9d56c211afb6c0f9d85450a
Hello! <angadsaini@proton.me>
2024-02-27 19:04:03 +0530

    a

--- messing around/script.js
+++ messing around/script.js
@@ -1,4 +1,3 @@
-document.getElementById("Join_now").addEventListener("click", function()
-{
-    window.location.href="https://youtu.be/dQw4w9WgXcQ?si=wxm0pO4rxEygRUBe";
+document.getElementById("join_now_button").addEventListener("click", function() {
+    window.open("https://youtu.be/dQw4w9WgXcQ?si=wxm0pO4rxEygRUBe", "_blank");
 });

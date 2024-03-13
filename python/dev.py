2a8deb3043455b56e306e98b9428ddeefc3a75d8
Hello! <angadsaini@proton.me>
2024-03-14 03:24:16 +0530

    open my vscode,git repo and github app

---
+++ python/dev.py
@@ -0,0 +1,11 @@
+import webbrowser
+import os
+
+chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe"
+
+os.system("code")
+
+url="https://github.com/UchihaSuscat/Web-dev-Learning-shit"
+webbrowser.open(url)
+
+os.system("Github Desktop")

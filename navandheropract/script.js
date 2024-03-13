484ce897c86278651602a77698bb749b5dfdf7fa
Hello! <angadsaini@proton.me>
2024-03-14 03:04:47 +0530

    Fun

---
+++ navandheropract/script.js
@@ -0,0 +1,7 @@
+document.getElementById('logo').addEventListener('click', function() {
+    window.location.href='index.html'
+})
+
+document.getElementById('pur').addEventListener('click', function(){
+    window.location.href='scam.html'
+})

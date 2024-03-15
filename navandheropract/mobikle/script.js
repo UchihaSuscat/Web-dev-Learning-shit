638332a301983f7620796022fefc35866d09bd28
Hello! <angadsaini@proton.me>
2024-03-15 22:29:13 +0530

    added welcome section

---
+++ navandheropract/mobikle/script.js
@@ -0,0 +1,11 @@
+document.getElementById('logo').addEventListener('click', function() {
+    window.location.href='index.html'
+})
+
+document.getElementById('pur').addEventListener('click', function() {
+    window.location.href='cart.html'
+})
+
+document.getElementById('cart').addEventListener('click', function() {
+    window.location.href='cart.html'
+})
